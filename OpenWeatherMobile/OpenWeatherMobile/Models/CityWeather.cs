734b6142c55bc7da256508b76243f6053af95693
mrpeny <bence.konya@captaincode.eu>
2018-04-30 18:23:46 +0200

    feat: Add Models

---
+++ OpenWeatherMobile/OpenWeatherMobile/Models/CityWeather.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace OpenWeatherMobile.Models
+{
+    [DataContract]
+    class CityWeather
+    {
+        public CityWeather() { }
+
+        [DataMember(Name = "cod")]
+        public int Cod { get; set; }
+        [DataMember(Name = "message")]
+        public string Message { get; set; }
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+        [DataMember(Name = "main")]
+        public MainWeather mainWeather { get; set; }
+    }
+}
---
+++ OpenWeatherMobile/OpenWeatherMobile/Models/MainWeather.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace OpenWeatherMobile.Models
+{
+    [DataContract]
+    class MainWeather
+    {
+        [DataMember(Name = "temp")]
+        public double Temp { get; set; }
+    }
+}
