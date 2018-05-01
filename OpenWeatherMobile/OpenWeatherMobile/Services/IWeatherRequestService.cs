ae51a7a0937a804e940dd41756378bf077e0d521
mrpeny <bence.konya@captaincode.eu>
2018-05-01 12:45:08 +0200

    feat: Create and implement IWeatherRequestService

---
+++ OpenWeatherMobile/OpenWeatherMobile/Services/IWeatherRequestService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenWeatherMobile.Services
+{
+    interface IWeatherRequestService
+    {
+        Task<Double> getTemperatureByCity(String cityName);
+    }
+}
---
+++ OpenWeatherMobile/OpenWeatherMobile/Services/OpenWeatherRequestService.cs
@@ -0,0 +1,34 @@
+﻿using OpenWeatherMobile.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenWeatherMobile.Services
+{
+    class OpenWeatherRequestService : IWeatherRequestService
+    {
+        private OpenWeatherNetworkUtils openWeatherNetworkUtils;
+        private OpenWeatherJsonParser openWeatherJsonParser;
+
+        public OpenWeatherRequestService(OpenWeatherNetworkUtils networkUtils, OpenWeatherJsonParser jsonParser)
+        {
+            this.openWeatherNetworkUtils = networkUtils;
+            this.openWeatherJsonParser = jsonParser;
+        }
+
+
+        async public Task<Double> getTemperatureByCity(string cityName)
+        {
+            var jsonTask = await openWeatherNetworkUtils.GetCityWeatherJson("Budapest");
+            CityWeather cityWeather = openWeatherJsonParser.parseCityWeatherFrom(jsonTask);
+            if (cityWeather == null)           
+            {
+                Console.WriteLine("CityWeather null");
+                return Double.NaN;
+            }
+
+            return cityWeather.MainWeather.Temp;     
+        }
+    }
+}
